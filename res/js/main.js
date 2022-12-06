4b6fcdad1314a830220f4dc4abc2148db0cf5232
deesdav <105449474+deesdav@users.noreply.github.com>
2022-12-06 18:07:48 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -760,7 +760,7 @@ portalsWarningContinue.onclick = () => {
     backButtonProblem.style.display = "block";
     heroAttack.style.display = "none";
     heroIdle.style.display = "none";
-    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundImage = "url(./res/img/heavenBackground.jpg)";
     merchantOffers.style.display = "none";
     yourHP.style.display = "none";
     backButtonProblem.style.opacity = "1";
